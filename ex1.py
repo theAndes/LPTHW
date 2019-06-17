803b42397318399030d4b3c882186d0c521c92ed
theAndes <age.esquivel@gmail.com>
2019-06-17 17:18:57 -0500

    Exercise 1: A Good First Program

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# Exercise 1: A Good First Program
+# https://learnpythonthehardway.org/python3/ex1.html
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing')
+print("I'd rather you 'not'.")
+print('I "said" do not touch this.')
