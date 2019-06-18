d366f0662d4d85e5163b4e863be8cf41f290b8eb
theAndes <age.esquivel@gmail.com>
2019-06-17 19:26:28 -0500

    Study Drills Above each line, use the # to write a comment to yourself explaining what the line does.

--- ex3.py
+++ ex3.py
@@ -13,14 +13,27 @@
 
 print("I will now count my chickens:")
 
+# Order of operation:
+# First: 30 / 6 = 5
+# Nest: 25 + 5 = 30
 print("Hens", 25 + 30 / 6)
+
+# First: 25 * 3 = 75
+# Next: 75 % 4 = 3  <<< 4 * 18 = 72 >>> 3 is remainder of 75 - 72
+# Then: 100 - 3 = 97
 print("Roosters", 100 - 25 * 3 % 4)
 
 print("I will now count my eggs:")
 
+# 3 + 2 + 1 (- 5) + (4 % 2) - (1 / 4) + 6
+# 3 + 2 + 1 ((- 5) + 0) - .25 + 6
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
 print("Is it true that 3 + 2 < 5 -7?")
+# Evaluate TRUE/FALSE
+# First: 3 + 2 = 5
+# Next: 5 - 7 = -2
+# 5 is less than - 2 : FALSE!
 print(3 + 2 < 5 - 7)
 
 print("What is 3 + 2 ?", 3 + 2)
